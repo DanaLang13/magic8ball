504cac0cbe2b416bd460ca273e7763049ad1b399
DanaLang13 <107574542+DanaLang13@users.noreply.github.com>
2022-10-25 15:37:17 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-
+function GenerateAnswer() {
+    responses = [
+      'It is decidedely so',
+      'No',
+      'Yes definitely',
+      'You may rely on it',
+      'Without a doubt',
+      'Very doubtful',
+      'Most likely',
+      'Outlook good',
+      'Yes',
+      'Signs point to yes',
+      'Reply hazy, try again',
+      'Ask again later',
+      'Better not tell you now',
+      'Cannot predict now',
+      'Concentrate and as again',
+      'Dont count on it',
+      'My reply is no',
+      'My sources say no',
+      'Outlook not so good',
+    ];
+    response = responses[Math.floor(Math.random() * responses.length)];
+    document.getElementById('outputDiv').innerHTML = response;
+  }
